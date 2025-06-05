6bd389b185f968feacb3344754866265f2bed142
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2025-06-06 01:27:31 +0530

    remove some files

--- intervuaiproject/app/layout.tsx
+++ intervuaiproject/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,9 +32,11 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${monaSans.className} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${monaSans.className} antialiased pattern`}
       >
         {children}
+
+        <Toaster />
       </body>
     </html>
   );
